d68d10457e3f405ce82db61c58b7d3069dd8c28c
kachinne <nobbnet@gmail.com>
2022-09-09 09:36:09 -0700

    Software now we come

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints all the letters except q and e
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char look;
+
+	for (look = 'a'; look <= 'z'; look++)
+		if (look == q && e)
+			putchar (look);
+	return (0);
+}
+
