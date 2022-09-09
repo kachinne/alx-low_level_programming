664e1eb773b8df81673c9416d5a6f94b8a4622cd
kachinne <nobbnet@gmail.com>
2022-09-09 09:41:35 -0700

    Software is for the men

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,10 @@ int main(void)
 	char look;
 
 	for (look = 'a'; look <= 'z'; look++)
-		if (look == q && e)
+	{
+		if (look != 'q' && look != 'e')
 			putchar (look);
+
 	putchar('\n');
 	return (0);
 }
