56ce5ef4f36155d8ba5d4f7b3c987bb36351e179
kachinne <nobbnet@gmail.com>
2022-09-09 10:29:22 -0700

    Printing c programming with putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet in reverse other
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 'z'; letter < 'a'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
