f8a71e08761ea51eb26c9dfab66dea817d6a8c53
kachinne <nobbnet@gmail.com>
2022-09-13 11:16:49 -0700

    The work of for loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void);
+{
+int num;
+
+for (num = 'a'; num <= 'z' num++)
+_putchar('num');
+-putchar('\n');
+return (0);
+}
+
