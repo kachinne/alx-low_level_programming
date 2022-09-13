b875df36c3acb10f943d7ac1ed125ce2a25fd959
kachinne <nobbnet@gmail.com>
2022-09-13 12:41:57 -0700

    Save us all

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,14 @@ void print_alphabet_x10(void)
 	int num;
 	int num1;
 
-	for (num = 'a'; num <= 'z'; num++)
+	for (num = 1; num <= 10; num++)
 	{
-		for (num1 = 1; num1 <= 10; num1++)
+		for (num1 = 'a'; num1 <= 'z'; num1++)
 		{
 			_putchar(num);
 		}
 _putchar('\n');
 	}
-return (0);
+
 
 }
