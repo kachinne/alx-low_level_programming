d05e2ffdd7ea48afefb4cce04827532a22664687
kachinne <nobbnet@gmail.com>
2022-09-13 14:58:39 -0700

    Nwa mmadu

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Integer that computes the absolute value
+ * @a: Parameter
+ *
+ * Return: Always 0
+ */
+int _abs(int a)
+{
+	if (a > 0)
+	{
+		a = -a;
+		return (-a);
+	}
+}
+
