959d11cdc034fa2c8a9239dc8a6f230b19821416
kachinne <nobbnet@gmail.com>
2022-09-13 11:01:20 -0700

    The kind work umu agbara dey am

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,14 +9,16 @@
 int main(void)
 
 {
-	char peace = '_putchar';
 
-	while peace
-	{
-		_putchar('peace');
-	peace++;
-	}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	_putchar('\n');
 return (0);
 }
