622872d3efc556be80552ab128e727250ac18be3
kachinne <nobbnet@gmail.com>
2022-09-15 07:30:46 -0700

    People once

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - printing numbers well
+ *
+ * Return: Always 0
+ *
+ */
+void more_numbers(void)
+{
+	int num;
+	{
+		for (num = 0; num <= 14; num++)
+			_putchar();
+		_putchar();
+		_putchar('\n'));
+	}
+}
