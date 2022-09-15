eb889ea9ec58554465c4fb0e3ff23ee12068a4a9
kachinne <nobbnet@gmail.com>
2022-09-15 05:27:40 -0700

    What is done is good

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - printing of numbers
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int num = '9';
+
+	for (num >= '0' && num <= '9')
+		_putchar('num');
+	_putchar('\n');
+	return (0);
+}
