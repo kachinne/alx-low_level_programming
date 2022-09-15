6f405cc9819741ca6c1d4a57a04318017ded6638
kachinne <nobbnet@gmail.com>
2022-09-15 09:10:33 -0700

    Believe in yourself

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_square - Function that prints square
+ * @size: parameter
+ *
+ * Return: Always 0
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('#');
+			_putchar('\n');
+}
+
