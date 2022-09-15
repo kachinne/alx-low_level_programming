d614abafa0decc787cb345deb8a614793c73d097
kachinne <nobbnet@gmail.com>
2022-09-15 04:10:16 -0700

    Give it your all

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is positive or negative
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+
+{
+	int i;
+
+	i = 98;
+	printf("%d\n", i);
+	return (0);
+}
