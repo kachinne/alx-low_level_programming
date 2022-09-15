891664ef60bc1efa44b4dffffbe9ccd38260315c
kachinne <nobbnet@gmail.com>
2022-09-15 05:32:59 -0700

    Testing work

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,10 +6,10 @@
  */
 void print_numbers(void)
 {
-	int num = '9';
+	int num
 
-	for (num >= '0' && num <= '9')
-		_putchar('num');
+	for (num = 0;  num <= 9; num++)
+		_putchar((num % 10) + '0');
 	_putchar('\n');
 	return (0);
 }
