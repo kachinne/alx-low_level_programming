27655143b197208be34974dc71aac3546898dfb5
kachinne <nobbnet@gmail.com>
2022-09-15 10:33:57 -0700

    Winning the battle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - printing a triangle with a new line
+ * @size: parameter
+ *
+ * Return: Always 0
+ */
+void print_triangle(int size)
+{
+	int i, b;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (b = size - i; b > 0; b--)
+				_putchar(' ');
+			for (b = 0; b < i; b++)
+				_putchar('#');
+			if (i == size)
+				continue;
+			}
+	}
+	_putchar('\n');
+}
