c0f11d652b45ce846e02379390ca507bccd764fd
kachinne <nobbnet@gmail.com>
2022-09-15 07:43:54 -0700

    Mmiri maru Ugo

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,17 @@
  */
 void more_numbers(void)
 {
-	int num;
+	int num, step;
 	{
-		for (num = 0; num <= 14; num++)
-			_putchar();
-		_putchar();
+		for (num = 0; num <= 9; num++)
+		{
+			for (step = 0; step <= 14; step++)
+			{
+				if (num > 10)
+					_putchar((num / 10) + '0');
+		_putchar((num % 10) + '0'));
+			}
 		_putchar('\n'));
 	}
+}
 }
