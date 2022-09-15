060468a66e04d34ee5cb459d6fb15567597f6133
kachinne <nobbnet@gmail.com>
2022-09-15 12:09:22 -0700

    Last for the day

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,2 +1,16 @@
 #include "main.h"
-
+/**
+ * print_number - Function that prints integer
+ * @n: parameter
+ *
+ * Return: Always 0
+ */
+void print_number(int n)
+{
+	int q;
+	:
+	_putchar('Enter an integer\n');
+	_putchar('%d', &q);
+	_putchar('The integer is: %d\n', q);
+	return (0);
+}
