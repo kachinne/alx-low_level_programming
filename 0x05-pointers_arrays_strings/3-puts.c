d41727274f2923a78393bf12c015677f144e53a6
kachinne <nobbnet@gmail.com>
2022-09-19 06:46:03 -0700

    Well loaded

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - Prints a string with a new line
+ * @str: paramter
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	char *str;
+
+	str = " I am well loaded in programming"
+		_puts(str);
+	_puts('\n');
+	return (0);
+}
