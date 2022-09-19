fe3d61ddff9bfed90c5622d19ac3d984a314a28f
kachinne <nobbnet@gmail.com>
2022-09-19 09:09:32 -0700

    Making it work

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,8 @@
  */
 void print_rev(char *s)
 {
-	int i, length, temp;
+	char s;
 
-	length = strlen(s);
-	for (i = 0; i < length / 2; i++)
-	{
-		temp = s[i];
-		s[i] = s[length - i - 1];
-		s[length - i - 1] = temp;
+	for (s = 1; s >= 10; s--)
 		_putchar('\n');
-}
 }
