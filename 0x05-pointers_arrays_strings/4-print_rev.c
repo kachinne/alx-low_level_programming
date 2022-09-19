bc9e03ed70b68b3041f24807d8a827101deb365a
kachinne <nobbnet@gmail.com>
2022-09-19 07:01:24 -0700

    Working in the blue

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse form
+ * @s: parameter
+ *
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	while (*s)
+		_putchar(*s--);
+	_putchar('\n');
+}
