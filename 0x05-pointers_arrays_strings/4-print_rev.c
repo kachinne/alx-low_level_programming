b5a4e586af527dfa49ede3e14e6c46e6d1838a36
kachinne <nobbnet@gmail.com>
2022-09-19 09:05:00 -0700

    Giving it your all

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,7 +7,14 @@
  */
 void print_rev(char *s)
 {
-	while (*s)
-	_putchar(*s--);
-	_putchar('\n');
+	int i, length, temp;
+
+	length = strlen(s);
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+		_putchar('\n');
+}
 }
