8fd010a833f47882bcdf02448f0888a6186fc045
kachinne <nobbnet@gmail.com>
2022-09-19 06:33:16 -0700

    Length of the work

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string
+ * @s: Parameter
+ *
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	char *s;
+	int len;
+
+	s = "Looking good";
+	len = _strlen(s);
+}
