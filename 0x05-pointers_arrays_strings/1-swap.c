af967bb89efb23133ad376bd32740df3d5ad85df
kachinne <nobbnet@gmail.com>
2022-09-19 06:10:25 -0700

    Join and swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: parameter first
+ * @b: parameter Second
+ *
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	double a, b, temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+	_putchar('temp');
+	_puchar ('\n');
+return (0);
+}
