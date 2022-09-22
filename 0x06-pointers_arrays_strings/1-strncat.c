6429c58f6a81e5496d2843901f7ed67bf3a4b52d
kachinne <nobbnet@gmail.com>
2022-09-22 13:17:19 -0700

    Umu Igbo

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - function to concatenate two strings
+ * @dest: Parameter one
+ * @src: Parameter two
+ * @n: Parameter three
+ *
+ * Return: Always 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int spark, length, n;
+	{
+		while (dest[spark++]
+				length++;
+				for (spark = n; src[spark]; spark++)
+				dest[length++] = src[spark]
+				return (dest);
+				}
+				}
