0328a49c1af2e70c0525bd59d59b5be89fb1fe98
kachinne <nobbnet@gmail.com>
2022-10-06 12:25:33 -0700

    making it work

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - allocation of memory
+ * @b: Parameter one
+ *
+ * Return: Always 0
+ */
+void *malloc_checked(unsigned int b);
+{
+	char *ant
+		if (b == 0)
+		{
+			exit(98);
+		}
+	ant = malloc(b);
+	return (ant);
+
