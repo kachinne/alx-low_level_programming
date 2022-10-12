a80a3107762939e7fafac2bb75819bb865c9eeea
kachinne <nobbnet@gmail.com>
2022-10-12 12:02:58 -0700

    Saving my people

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
-#include <stdio.h>
 /**
  * print_name - printing my name
  * @name: name of the chairman
+ * @f: feel free
  *
  * Return: nothing
  */
-void print_name(char *name, void (*f)(char*))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
