6aef8b94090380a48dd45ca864c31bff89889a62
kachinne <nobbnet@gmail.com>
2022-10-12 12:47:07 -0700

    Rasta man

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,41 +3,28 @@
 #include <stdio.h>
 
 /**
- * main - Prints the result of simple operations.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0.
+ * get_op_func - select the correct operation function asked by user
+ * @s: operator argument
+ * Return: function pointer corresponding to operator given
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int (*get_op_func(char *s))(int, int)
 {
-	int num1, num2;
-	char *op;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-	if (argc != 4)
+	i = 0;
+	while (i < 5)
 	{
-		printf("Error\n");
-		exit(98);
+		if (ops[i].op[0] == s[0])
+			return (ops[i].f);
+		i++;
 	}
-
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
-
-	if (get_op_func(op) == NULL || op[1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	printf("%d\n", get_op_func(op)(num1, num2));
-
-	return (0);
+	return (NULL);
 }
