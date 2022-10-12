56f6d0da5c6491f828a01b1b4df39915b570cafe
kachinne <nobbnet@gmail.com>
2022-10-12 11:56:02 -0700

    Changing world

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - printing my name
+ * @name: name of the chairman
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char*))
+{
+	if (name == NULL || f == NULL)
+		return;
+f(name);
+}
