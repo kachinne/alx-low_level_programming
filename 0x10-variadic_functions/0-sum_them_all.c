af8da8be4ab42ebe332ddb589467ef921b73dc87
kachinne <nobbnet@gmail.com>
2022-10-13 11:29:30 -0700

    Check

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+/** 
+ * sum_them_all - check the total
+ * @n: Bring it up
+ *
+ * Return: Always zero
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int num1, num2 = n;
+	if (n == 0)
+		return (0);
+}
